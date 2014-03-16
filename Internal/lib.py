a47108b0636142c86981227fb8f6d0982ee3b3c1
nxi <nxi@NBI0012D.nbi.ansto.gov.au>
2014-03-17 10:43:01 +1100

    update reduction scripts

--- Internal/lib.py
+++ Internal/lib.py
@@ -18,12 +18,14 @@ def make_eff_map(df, path):
     global LOWER_LIM
     
     map = df[path]
+    title = map.title
     if map.ndim == 4:
         map = map[0,0]
     elif map.ndim == 3:
         map = map[0]
     
     map = map.float_copy()
+    map.title = title
     map[0:SKIP_LEFT, :] = 1
     shape = map.shape
     map[shape[0] - SKIP_RIGHT : shape[0], :] = 1
@@ -40,13 +42,15 @@ def make_eff_map(df, path):
 def eff_corr(ds, map):
     for fm in ds:
         fm /= map
+    ds.append_log('Processed with: efficiency correction with ' + str(map.title))
 
-def geo_corr(ds):
-    sdds = DEFAULT_SAMPLE_TO_DETECTOR_DISTANCE ** 2
+def geo_corr(ds, enabled):
+    sdd = DEFAULT_SAMPLE_TO_DETECTOR_DISTANCE
     try:
-        sdds = ds.sample_to_detector_distance ** 2
+        sdd = ds.sample_to_detector_distance
     except:
         pass
+    sdds = sdd ** 2
     stth = ds.stth
     if not hasattr(stth, '__len__') :
         stth = [stth]
@@ -60,42 +64,150 @@ def geo_corr(ds):
     
     y_len = ds.axes[-2].size - 1
     x_len = ds.axes[-1].size - 1
-    d_shape = [y_len, x_len]
-    pixel_2theta = array.instance([y_len, x_len + 1], dtype = float)
     
-    if is_fixed_stth :
-        res = dataset.instance(ds.shape, dtype = float)
+    if enabled:
+        d_shape = [y_len, x_len]
+        pixel_2theta = array.instance([y_len, x_len + 1], dtype = float)
         
-        y_bounds = ds.axes[-2].__iArray__
-        y_centres = AxisRecord.createCentres(y_bounds)
-        x_bounds = ds.axes[-1].__iArray__
-        GeometryCorrection.calculateTwoThetaPixel(sdds, x_bounds, cosStth, sinStth, \
-                                             y_centres, pixel_2theta.__iArray__)
-        axis_2theta = array.instance([x_len + 1], dtype = float)
-        GeometryCorrection.calculateTwoThetaAxis(sdds, x_bounds, cosStth, \
-                                             sinStth, axis_2theta.__iArray__)
-        relocateLeftIndexArray = array.instance(d_shape, dtype = int)
-        relocateRightIndexArray = array.instance(d_shape, dtype = int)
-        relocateLeftRateArray = array.instance(d_shape, dtype = float)
-        relocateRightRateArray = array.instance(d_shape, dtype = float)
-        relocateCounterArray = array.instance(d_shape, dtype = float)
-        
-        for i in xrange(ds.shape[0]):
-            GeometryCorrection.rebinWithTwoTheta(ds[i].__iArray__, ds[i].var.__iArray__, \
-                        res[i].__iArray__, res[i].var.__iArray__, pixel_2theta.__iArray__, \
-                        axis_2theta.__iArray__, relocateLeftIndexArray.__iArray__, \
-                        relocateRightIndexArray.__iArray__, relocateLeftRateArray.__iArray__, \
-                        relocateRightRateArray.__iArray__, relocateCounterArray.__iArray__, \
-                        i == 0)
-        res.axes[2] = axis_2theta
-        res.copy_metadata_shallow(ds)
-        res.append_log('geometry curve correction')
+        if is_fixed_stth :
+            res = dataset.instance(ds.shape, dtype = float)
+            
+            y_bounds = ds.axes[-2].__iArray__
+            y_centres = AxisRecord.createCentres(y_bounds)
+            x_bounds = ds.axes[-1].__iArray__
+            GeometryCorrection.calculateTwoThetaPixel(sdds, x_bounds, cosStth, sinStth, \
+                                                 y_centres, pixel_2theta.__iArray__)
+            axis_2theta = array.instance([x_len + 1], dtype = float)
+            GeometryCorrection.calculateTwoThetaAxis(sdds, x_bounds, cosStth, \
+                                                 sinStth, axis_2theta.__iArray__)
+            relocateLeftIndexArray = array.instance(d_shape, dtype = int)
+            relocateRightIndexArray = array.instance(d_shape, dtype = int)
+            relocateLeftRateArray = array.instance(d_shape, dtype = float)
+            relocateRightRateArray = array.instance(d_shape, dtype = float)
+            relocateCounterArray = array.instance(d_shape, dtype = float)
+            
+            for i in xrange(ds.shape[0]):
+                GeometryCorrection.rebinWithTwoTheta(ds[i].__iArray__, ds[i].var.__iArray__, \
+                            res[i].__iArray__, res[i].var.__iArray__, pixel_2theta.__iArray__, \
+                            axis_2theta.__iArray__, relocateLeftIndexArray.__iArray__, \
+                            relocateRightIndexArray.__iArray__, relocateLeftRateArray.__iArray__, \
+                            relocateRightRateArray.__iArray__, relocateCounterArray.__iArray__, \
+                            i == 0)
+            res.axes[0] = ds.axes[0]
+            res.axes[1] = ds.axes[1]
+            res.axes[2] = axis_2theta
+            res.axes[2].title = 'two theta'
+            res.copy_metadata_shallow(ds)
+            res.append_log('Processed with: geometry curve correction')
+            res.append_log('Processed with: calculating two theta on LDS=%.1f mm' % sdd)
+        else:
+            pass
+        return res
     else:
-        pass
-    return res
+        axis_2theta = array.instance([x_len + 1], dtype = float)
+        x_bounds = ds.axes[2].__iArray__
+        GeometryCorrection.calculateTwoThetaAxis(sdds, x_bounds, cosStth, \
+                                                 sinStth, axis_2theta.__iArray__)
+        ds.axes[2] = axis_2theta
+        ds.axes[2].title = 'two theta'
+        ds.append_log('Processed with: calculating two theta on LDS=%.1f mm' % sdd)
+        return ds
 
-def v_intg(ds):
-    return ds.intg(1)
-
+def v_intg(ds, masks):
+    if len(masks) > 0 :
+        x_axis = ds.axes[-1]
+        y_axis = ds.axes[-2]
+        res = dataset.instance([ds.shape[0], ds.shape[2]], float('NaN'), dtype=float)
+        for mask in masks:
+            y_iMin = int((mask.minY - y_axis[0]) / (y_axis[-1] - y_axis[0]) \
+                         * (y_axis.size - 1))
+            if y_iMin < 0 :
+                y_iMin = 0
+            if y_iMin >= y_axis.size:
+                continue
+            y_iMax = int((mask.maxY - y_axis[0]) / (y_axis[-1] - y_axis[0]) \
+                         * (y_axis.size - 1)) + 1
+            if y_iMax < 0:
+                continue
+            x_iMin = int((mask.minX - x_axis[0]) / (x_axis[-1] - x_axis[0]) \
+                         * (x_axis.size - 1))
+            if x_iMin < 0:
+                x_iMin = 0;
+            if x_iMin >= x_axis.size:
+                continue
+            x_iMax = int((mask.maxX - x_axis[0]) / (x_axis[-1] - x_axis[0]) \
+                         * (x_axis.size - 1)) + 1
+            if x_iMax < 0:
+                continue
+            res[:, x_iMin : x_iMax] = ds[:, y_iMin : y_iMax, x_iMin : x_iMax].intg(1)
+        res.axes[0] = ds.axes[0]
+        res.axes[1] = ds.axes[2]
+        mask = masks[0]
+        res.append_log(ds.log + 'Processed with: apply mask y in [' + str(mask.minY) + ',' \
+                       + str(mask.maxY) + '], x in [' + str(mask.minX) + ',' + str(mask.maxX) + ']')
+        ds.log = res.log
+        return res
+    else:
+        return ds.intg(1)
+            
 def i_intg(ds):
     return ds.sum(0)
+
+def v_export(ds, path):
+    sn = 'KWR%07d'%ds.id
+    ext = ''
+    log = ds.log
+    if log.__contains__('efficiency') :
+        ext += 'e'
+    if log.__contains__('geometry') :
+        ext += 'g'
+    if log.__contains__('y in [') :
+        minfo = log[log.index('y in [') : ]
+        minfo = minfo[6 : minfo.index(']')]
+        ext += '_' + minfo.replace(',', '_')
+    if len(ext) > 0:
+        sn += '_' + ext
+    sn += '.xyd'
+    
+    f = open(path + '/' + sn, 'w')
+    
+    if f is None:
+        print 'failed to make file ' + path + '/' + sn
+        
+    loc = ds.location
+    if loc.startswith('/') :
+        loc = loc[1:]
+    text = '# Raw nexus file: %s\n' % loc
+    text += '# \texperiment_title=%s \tsample_name=%s\n' % (str(ds.experiment_title), \
+                                                           str(ds.sample_name))
+    text += '# \tsample_description=%s\n' % str(ds.sample_description)
+    text += '# \tuser_name=%s\n' % str(ds.user_name)
+    text += '# DETECTOR resolution=(421,421) \tactive_width=280.0 mm \tmode=%s \tpreset=%s\n' % \
+                (str(ds.mode), str(ds.preset))
+    text += '# SAMPLE environment\n'
+    text += '# \tsx=%.5f mm \tsy=%.5f mm \tsz=%.5f mm\n' % (ds.sx[0], ds.sy[0], ds.sz[0])
+    text += '# \tsom=%.5f degrees \tstth=%.5f degrees\n' % (ds.som[0], ds.stth[0])
+    text += '# MONOCHROMATOR environment\n'
+    text += '# \tmphi=%.5f degrees \tmchi=%.5f degrees\n' % (ds.mphi[0], ds.mchi[0])
+    text += '# \tmx=%.5f mm \tmy=%.5f mm\n' % (ds.mx[0], ds.my[0])
+    text += '# \tmom=%.5f degrees \tmtth=%.5f degrees\n' % (ds.mom[0], ds.mtth[0])
+    text += '# \tmf1=%.5f degrees \tmf2=%.5f degrees\n' % (ds.mf1[0], ds.mf2[0])
+    text += '# SLITS environment\n'
+    text += '# \tpsp=%.5f mm \tpsw=%.5f mm \tpsho=%.5f mm\n' % (ds.psp[0], ds.psw[0], ds.psho[0])
+    text += '# \tssp=%.5f mm \tssho=%.5f mm\n' % (ds.ssp[0], ds.ssho[0])
+    text += '# ' + log.replace('\n', '\n# ') + '\n'
+    scan_var = ds.axes[0]
+    x_axis = ds.axes[1]
+    for i in xrange(ds.shape[0]):
+        text += '# Scan variable: %s=%f\n' % (scan_var.title, scan_var[i])
+        text += '# time=%f seconds \tbm1_counts=%d\n' % (ds.detector_time[i], ds.bm1_counts[i])
+        text += '# Two Theta \tIntensity Integration \tSigma\n'
+        for j in xrange(ds.shape[1]):
+            val = ds[i, j]
+            var = ds.var[i, j]
+            text += '%10.5f %15.5f %15.5f\n' % ((x_axis[j + 1] + x_axis[j]) / 2, \
+                                    (val if not math.isnan(val) else 0), \
+                                    (math.sqrt(var) if not math.isnan(var) and var != 0 else 1))
+        text += '\n'
+    f.write(text)
+    f.close()
--- Internal/reduction.py
+++ Internal/reduction.py
@@ -1,5 +1,6 @@
 from Internal import lib
 import math
+from gumpy.vis.event import MouseListener, MaskEventListener, AWTMouseListener
 
 # Script control setup area
 # script info
@@ -9,9 +10,65 @@ __script__.version = '1.0'
 # Use below example to create parameters.
 # The type can be string, int, float, bool, file.
 
+class NavMouseListener(MouseListener):
+    
+    def __init__(self):
+        MouseListener.__init__(self)
+        
+    def on_double_click(self, event):
+        x = event.getX()
+        idx = var_jump.options.index(x)
+        ind_jump.value = idx
+        
+    def on_click(self, event):
+        print "x=" + str(event.getX()) + "; y=" + str(event.getY()) + \
+                    "; use double click to jump to x=" + str(event.getX())
+    
+__mask_updated__ = False
+
+class RegionEventListener(MaskEventListener):
+    
+    def __init__(self):
+        MaskEventListener.__init__(self)
+    
+    def mask_added(self, mask):
+        pass
+            
+    def mask_removed(self, mask):
+        global __mask_updated__
+        update_mask_list()
+        __mask_updated__ = True
+    
+    def mask_updated(self, mask):
+        global __mask_updated__
+        update_mask_list()
+        __mask_updated__ = True
+        
+regionListener = RegionEventListener()
+
+class MousePressListener(AWTMouseListener):
+    def __init__(self):
+        AWTMouseListener.__init__(self)
+    
+    def mouse_released(self, event):
+        global __mask_updated__
+        if __mask_updated__ :
+            run_intg()
+            __mask_updated__ = False
+    
+mouse_press_listener = MousePressListener()
+
 DS = None
 VI = None
 IR = None
+
+INT_EXP_OPTIONS = ["default - use current mask", \
+                   "use the whole detector", \
+                   "split detector into 2 strips", \
+                   "split detector into 3 strips", \
+                   "split detector into 4 strips", \
+                   "split detector into 5 strips", \
+                   ]
 
 ind_jump = Par('int', -1, options = [], command = 'jump_to_index()')
 ind_jump.title = 'select to show data at index'
@@ -27,75 +84,153 @@ def jump_to_var():
         return
     idx = var_jump.options.index(var)
     ind_jump.value = idx
+
+def update_plots(idx):
+    Plot1.set_dataset(DS[idx])
+    Plot1.title = str(DS.id) + "_" + str(idx)
+    Plot2.set_dataset(VI[idx])
+    Plot2.title = str(DS.id) + "_integration_" + str(idx)
     
 def jump_to_index():
     idx = ind_jump.value
     log('jump to index ' + str(idx))
     if idx < 0 or DS == None or idx >= len(DS):
         return
-    Plot1.set_dataset(DS[idx])
-    Plot2.set_dataset(VI[idx])
+    update_plots(idx)
     if var_jump.value != var_jump.options[idx]:
         var_jump.value = var_jump.options[idx]
 
 eff_corr_enabled = Par('bool', True)
 eff_corr_enabled.title = 'efficiency correction enabled'
-eff_map = Par('file', '')
+eff_map = Par('file', u'W:\data\kowari\KWR0049477.nx.hdf')
 eff_map.title = 'efficiency map file'
 g_eff = Group('Efficiency Correction')
 g_eff.add(eff_corr_enabled, eff_map)
 
-geo_corr_enabled = Par('bool', True)
+geo_corr_enabled = Par('bool', False)
 geo_corr_enabled.title = 'geometry correction enabled'
 g_geo = Group('Geometry Correction')
 g_geo.add(geo_corr_enabled)
 
 reg_enabled = Par('bool', True)
 reg_enabled.title = 'region selection enabled'
+reg_list = Par('string', '')
+reg_list.title = 'mask list'
+reg_list.enabled = False
+
 g_reg = Group('Region Selection')
-g_reg.add(reg_enabled)
+g_reg.add(reg_enabled, reg_list)
 
 # Use below example to create a button
 act1 = Act('reduce()', 'Reduce Selected Data') 
+
+exp_mask = Par('str', INT_EXP_OPTIONS[0], options = INT_EXP_OPTIONS)
+exp_b = Act('integration_export()', 'Batch Export Integration Result')
+g_exp = Group('Integration Export')
+g_exp.add(exp_mask, exp_b)
+
+exp_i = Act('intensity_export()', 'Batch Export Intensity')
+g_int = Group('Intensity Export')
+g_int.add(exp_i)
+
+
 def reduce():
     global DS
     global VI
     global IR
+    
+    if Plot1.pv.getPlot() is None:
+        Plot1.set_dataset(instance([2,2]))
+    old_id = -1
+    if not DS is None:
+        old_id = DS.id 
     df.datasets.clear()
     li = __DATASOURCE__.getSelectedDatasets()
     if len(li) == 0:
         open_error('Please select a file from the file source view.')
         return
     DS = df[str(li[0].location)]
+    curr_idx = -1
+    if old_id == DS.id:
+        curr_idx = ind_jump.value
     location = DS.location
     id = DS.id
     title = DS.title
     DS = DS.get_reduced(1)
-    Plot1.set_dataset(DS[0])
     
     if eff_corr_enabled.value and eff_map.value != None \
             and len(eff_map.value.strip()) > 0:
-        log('efficiency correction')
+        log('running efficiency correction')
         map = lib.make_eff_map(df, str(eff_map.value))
         lib.eff_corr(DS, map)
         
     if geo_corr_enabled.value :
-        log('geometry correction')
-        DS = lib.geo_corr(DS)
-        
+        log('running geometry correction')
+    DS = lib.geo_corr(DS, geo_corr_enabled.value)
+
     DS.location = location
     DS.id = id
     DS.title = title
-    Plot1.set_dataset(DS[0])
+#    Plot1.set_dataset(DS[0])
+#    Plot1.title = str(DS.id) + '_0'
+#    Plot1.set_mask_listener(regionListener)
+        
+    masks = []
+    if reg_enabled.value :
+        masks = Plot1.get_masks()
+    
+    log('running vertical integration')
+    VI = lib.v_intg(DS, masks)
+#    Plot2.set_dataset(VI[0])
+#    Plot2.title = str(DS.id) + "_integration_0"
+
     ind_jump.options = range(DS.shape[0])
     var_jump.options = DS.axes[0].tolist()
-    ind_jump.value = 0
-        
-    VI = lib.v_intg(DS)
-    Plot2.set_dataset(VI[0])
+    if curr_idx == -1:
+        ind_jump.value = 0
+    else:
+        ind_jump.value = curr_idx
+        update_plots(curr_idx)
     
+    log('running intensity integration')
     IR = lib.i_intg(VI)
     Plot3.set_dataset(IR)
+    Plot3.title = str(DS.id) + "_intensity"
+    Plot3.set_mouse_listener(NavMouseListener())
+    
+    Plot1.set_awt_mouse_listener(mouse_press_listener)
+    Plot1.set_mask_listener(regionListener)
+    
+def run_intg():
+    global DS
+    global VI
+    global IR
+    log('running vertical integration')
+    masks = Plot1.get_masks()
+    VI = lib.v_intg(DS, masks)
+#    Plot2.set_dataset(VI[0])
+#    Plot2.title = str(DS.id) + "_integration_0"
+
+    ind_jump.options = range(DS.shape[0])
+    var_jump.options = DS.axes[0].tolist()
+    update_plots(ind_jump.value)
+    
+    log('running intensity integration')
+    IR = lib.i_intg(VI)
+    Plot3.set_dataset(IR)
+    Plot3.title = str(DS.id) + "_intensity"
+    Plot3.set_mouse_listener(NavMouseListener())
+    __mask_locked__ = False
+        
+#def apply_region(ds, masks):
+#    map = array.instance([ds.shape[-2], ds.shape[-1]], dtype=bool)
+#    rds = instance(ds.shape)
+#    x_axis = ds.axes[-1]
+#    y_axis = ds.axes[-2]
+#    for mask in masks :
+#        for i in xrange(len(x_axis)):
+#            if ix > 
+        
     
 # Use below example to create a new Plot
 # Plot4 = Plot(title = 'new plot')
@@ -103,19 +238,10 @@ def reduce():
 # This function is called when pushing the Run button in the control UI.
 def __run_script__(fns):
     # Use the provided resources, please don't remove.
-    global Plot1
-    global Plot2
-    global Plot3
+    reduce()
     
-    # check if a list of file names has been given
-    if (fns is None or len(fns) == 0) :
-        print 'no input datasets'
-    else :
-        for fn in fns:
-            # load dataset with each file name
-            ds = df[fn]
-            print ds.shape
-    print arg1_name.value
+def __dataset_added__(fn):
+    pass
     
 def __dispose__():
     global Plot1
@@ -124,3 +250,100 @@ def __dispose__():
     Plot1.clear()
     Plot2.clear()
     Plot3.clear()
+
+def precision_string(val, precision):
+    return '%.2f' % val
+    
+def update_mask_list():
+    if Plot1.ndim > 0:
+        reg_list.value = str(Plot1.get_masks())
+    
+update_mask_list()
+
+def silent_reduce(ds, map = None):
+    location = ds.location
+    id = ds.id
+    title = ds.title
+    ds = ds.get_reduced(1)
+    
+    if not map is None:
+        lib.eff_corr(ds, map)
+        
+    rds = lib.geo_corr(ds, geo_corr_enabled.value)
+    ds.close()
+    rds.id = id
+    return rds
+
+#    masks = []
+#    if reg_enabled.value :
+#        try:
+#            masks = Plot1.get_masks()
+#        except:
+#            pass
+#    
+#    vi = lib.v_intg(ds, masks)
+#
+#    vi.location = location
+#    vi.id = id
+#    vi.title = title
+#        
+#    ir = lib.i_intg(vi)
+#    return [vi, ir]
+    
+def integration_export():
+    global INT_EXP_OPTIONS
+    path = selectSaveFolder()
+    if path == None:
+        return
+    dss = __DATASOURCE__.getSelectedDatasets()
+    if len(dss) == 0:
+        return
+    fi = File(path)
+    if not fi.exists():
+        if not fi.mkdir():
+            print 'Error: failed to make directory: ' + path
+            return
+    if eff_corr_enabled.value and eff_map.value != None \
+            and len(eff_map.value.strip()) > 0:
+        map = lib.make_eff_map(df, str(eff_map.value))
+    else:
+        map = None
+    for dinfo in dss:
+        df.datasets.clear()
+        log('exporting ' + dinfo.location)
+        ds = df[str(dinfo.location)]
+        rds = silent_reduce(ds, map)
+        masks = []
+        if exp_mask.value == INT_EXP_OPTIONS[0]:
+            if reg_enabled.value :
+                try:
+                    masks = Plot1.get_masks()
+                except:
+                    pass
+            vi = lib.v_intg(rds, masks)
+            vi.location = ds.location
+            vi.copy_metadata_shallow(rds)
+            lib.v_export(vi, path)
+        elif exp_mask.value == INT_EXP_OPTIONS[1]:
+            vi = lib.v_intg(rds, masks)
+            vi.copy_metadata_shallow(rds)
+            lib.v_export(vi, path)
+        else :
+            idx = INT_EXP_OPTIONS.index(exp_mask.value)
+            mask_group = make_mask_group(rds, idx)
+            for mask in mask_group :
+                vi = lib.v_intg(rds, [mask])
+                vi.copy_metadata_shallow(rds)
+                lib.v_export(vi, path)
+    print 'Done'
+
+def make_mask_group(ds, num):
+    masks = []
+    x_axis = ds.axes[1]
+    y_axis = ds.axes[0]
+    y_step = (y_axis[-1] - y_axis[0]) / num
+    for i in xrange(num) :
+        mask = RectangleMask(True, x_axis[0], y_axis[0] + y_step * i, \
+                             x_axis[-1] - x_axis[0], y_step)
+        masks.append(mask)
+    return masks
